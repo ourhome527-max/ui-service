1171a71d680db3ae89b25f0f99e2c21e3066e844
ourhome527 <ourhome527@gmail.com>
2025-11-19 10:59:05 +0900

    templates modify

--- src/main/java/com/example/controller/ViewController.java
+++ src/main/java/com/example/controller/ViewController.java
@@ -3,11 +3,15 @@ package com.example.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import lombok.extern.slf4j.Slf4j;
+
 @Controller
+@Slf4j
 public class ViewController {
 
 	@GetMapping("/user/login-user")
 	public String getLoginPage() {
+		log.info("getLoginPage 메서드 실행");
 		return "login-user";
 	}
 }
