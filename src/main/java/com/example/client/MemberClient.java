b05f5beb87292be04a4d96736e478db07c57660a
ourhome527 <ourhome527@gmail.com>
2025-11-20 09:40:15 +0900

    modify

--- src/main/java/com/example/client/MemberClient.java
+++ src/main/java/com/example/client/MemberClient.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import com.example.domain.Member;
 import com.example.domain.dto.UserLoginReq;
 
-@FeignClient(name = "member-api", url = "http://member-service:8093/api/members")
+@FeignClient(name = "member-api", url = "http://member-service:8081/api/members")
 public interface MemberClient {
 	@PostMapping("/login")
 	public ResponseEntity<Member> login(@RequestBody UserLoginReq request);
