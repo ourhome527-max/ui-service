b745b8f77c70c7047f353542cbcc5b0c4254192c
ourhome527 <ourhome527@gmail.com>
2025-11-28 12:24:18 +0900

    commit

--- src/main/java/com/example/client/ArticleClient.java
+++ src/main/java/com/example/client/ArticleClient.java
@@ -6,6 +6,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -27,7 +28,7 @@ public interface ArticleClient {
 
 	// 변경점: consumes 설정 추가, @RequestBody -> @RequestPart로 변경, 파일 파라미터 추가
 	@PostMapping(value = "/regist", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-	ResponseEntity<Void> registArticle(@RequestPart("article") RegistArticleReq request,
+	ResponseEntity<Void> registArticle(@ModelAttribute RegistArticleReq request,
 			@RequestPart(value = "files", required = false) List<MultipartFile> files);
 
 	@GetMapping("/{articleId}")
